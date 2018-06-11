afc81f6e9c49e6f957c8f23885a425d56fd56495
Adam Palay <adampalay@gmail.com>
2018-06-11 18:14:46 -0400

    started down the concatenation route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#![recursion_limit = "8"]
+#![recursion_limit = "16"]
+
 use std::marker::PhantomData;
 
 fn main() {
@@ -6,7 +7,12 @@ fn main() {
     // let () = <() as SKI<Seq<K, Seq<I, Seq<I, Empty>>>>>::reduce(PhantomData);
     // let () = <() as SKI<Seq<K, Seq<I, Empty>>>>::reduce(PhantomData);
     // let () = <() as SKI<Seq<K, Empty>>>::reduce(PhantomData);
-    let () = <() as SKI<Seq<S, Seq<I, Seq<I, Seq<I, Empty>>>>>>::reduce(PhantomData);
+    // let () = <() as SKI<Seq<S, Seq<I, Seq<I, Seq<I, Empty>>>>>>::reduce(PhantomData);
+    // let () = <() as SKI<Seq<S, Seq<I, Seq<I, Seq<I, Seq<I, Empty>>>>>>>::reduce(PhantomData);
+    let () = <() as SKI<Seq<Group<Seq<I, Empty>>, Seq<K, Empty>>>>::reduce(PhantomData);
+    // let () = <() as SKI<
+    //     Seq<S, Seq<I, Seq<I, Seq<Group<Seq<S, Seq<I, Seq<I, Empty>>>>, Empty>>>>,
+    // >>::reduce(PhantomData);
 }
 
 // * symbols
@@ -29,7 +35,7 @@ impl Element for Z {}
 
 // * list
 
-struct Seq<A: Element, B> {
+struct Seq<A: Element, B: List> {
     _phantom: PhantomData<(A, B)>,
 }
 
@@ -38,6 +44,15 @@ struct Empty;
 trait List {}
 impl<A: Element, Rest: List> List for Seq<A, Rest> {}
 impl List for Empty {}
+
+// concatenation
+trait Concat<A: List, B: List> {
+    type Result: List;
+
+    fn concat(PhantomData: PhantomData<(A, B)>) -> PhantomData<Self::Result> {
+        PhantomData
+    }
+}
 
 // * groups
 
@@ -50,7 +65,7 @@ impl<Inner: List> Element for Group<Inner> {}
 // * SKI reduction
 
 trait SKI<L: List> {
-    type Result;
+    type Result: List;
 
     fn reduce(PhantomData: PhantomData<L>) -> PhantomData<Self::Result> {
         PhantomData
@@ -106,14 +121,37 @@ where
     type Result = <() as SKI<Seq<A, Seq<C, Seq<Group<Seq<B, Seq<C, Empty>>>, Empty>>>>>::Result;
 }
 
+impl SKI<Seq<S, Empty>> for () {
+    type Result = Seq<S, Empty>;
+}
+
+impl<A> SKI<Seq<S, Seq<A, Empty>>> for ()
+where
+    A: Element,
+{
+    type Result = Seq<S, Seq<A, Empty>>;
+}
+
+impl<A, B> SKI<Seq<S, Seq<A, Seq<B, Empty>>>> for ()
+where
+    A: Element,
+    B: Element,
+{
+    type Result = Seq<S, Seq<A, Seq<B, Empty>>>;
+}
+
 // * Group reduction rule
 impl<Expr, Rest> SKI<Seq<Group<Expr>, Rest>> for ()
 where
     Expr: List,
     Rest: List,
     (): SKI<Expr>,
-    // (): SKI<Seq<<() as SKI<Expr>>::Result, Rest>>,
+    // <() as SKI<Expr>>::Result: List,
+    (): Concat<<() as SKI<Expr>>::Result, Rest>,
+    (): SKI<<() as Concat<<() as SKI<Expr>>::Result, Rest>>::Result>,
+    // (): SKI<Seq<<() as SKI<Expr>>::Result, Rest>>
 {
-    type Result = <() as SKI<Expr>>::Result;
+    // type Result = Seq<<() as SKI<Expr>>::Result, Rest>;
     // type Result = <() as SKI<Seq<<() as SKI<Expr>>::Result, Rest>>>::Result;
+    type Result = <() as SKI<<() as Concat<<() as SKI<Expr>>::Result, Rest>>::Result>>::Result;
 }
